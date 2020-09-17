4b8c9b524283ec36e8db3a3f4c8cc603a4a42698
Michael Keyes <michael.d.keyes87@gmail.com>
2020-09-17 14:34:52 -0400

    Scaffolding and setup

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Working!");
+import { LEVEL, OBJECT_TYPE } from "./setup";
+
+// DOM Elements
+const gameGrid = document.querySelector("#game");
+const scoreTable = document.querySelector("#score");
+const startButton = document.querySelector("#start-button");
+
+// Game Constants
+const POWER_PILL_TIME = 10000;
+const GLOBAL_SPEED = 80;
+
+// Initial Setup
+let score = 0;
+let timer = null;
+let gameWin = false;
+let powerPillActive = false;
+let powerPillTimer = null;
+
+function gameOver(pacman, grid) {}
+
+function checkCollision(pacman, ghosts) {}
+
+function gameLoop(pacman, ghosts) {}
+
+function startGame() {}
